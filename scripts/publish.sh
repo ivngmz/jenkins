3328f3fea64c8f32fd1f18f4d3f57d870336e4f9
ivan <ivngmz2018@gmail.com>
2022-06-01 17:30:06 +0200

    test

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,5 +5,5 @@ sha='093423jdskdfs2314'
 component_label="$1-$2"
 
 git branch --show-current
-git fetch && git pull && git checkout main
+git fetch && git checkout main && git pull
 gh pr comment ${PR} --body "### $component_label:$tag:$sha ###"
