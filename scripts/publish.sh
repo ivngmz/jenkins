d9100aa779955334f7aa235ec4c35e9c112d4db0
ivan <ivngmz2018@gmail.com>
2022-06-01 18:00:05 +0200

    test

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,8 +4,8 @@ tag='testtag'
 sha='093423jdskdfs2314'
 component_label="$1-$2"
 
-git branch --show-current
-git fetch && git checkout main && git pull
-echo "gh api /repos/ivngmz/jenkins/issues/${PR}/comments -f body='Hi from CLI'"
+#git branch --show-current
+#git fetch && git checkout main && git pull
+#echo "gh api /repos/ivngmz/jenkins/issues/${PR}/comments -f body='Hi from CLI'"
 gh api /repos/ivngmz/jenkins/issues/${PR}/comments -f body='Hi from CLI'
 #gh pr comment ${PR} --body "### $component_label:$tag:$sha ###"
